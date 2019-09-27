8834a819ba06e36c5a837da43ea9b8a22f576180
Eric Oestrich <eric@oestrich.org>
2019-09-27 15:27:47 -0400

    Move to capitalized HCL

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule Hcl.MixProject do
+defmodule HCL.MixProject do
   use Mix.Project
 
   def project do
--- test/hcl_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule HclTest do
-  use ExUnit.Case
-  doctest Hcl
-
-  test "greets the world" do
-    assert Hcl.hello() == :world
-  end
-end
